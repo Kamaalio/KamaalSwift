f553617ce4d4790d3532969ca6bfb2e5100ed0b2
Kamaal <kamaal.f1@gmail.com>
2023-04-21 19:03:01 +0200

    Building example app in CI

--- Example/Example/ContentView.swift
+++ Example/Example/ContentView.swift
@@ -9,24 +9,142 @@ import SwiftUI
 import KamaalUI
 import KamaalLogger
 import KamaalExtensions
+import KamaalNavigation
 
 private let logger = KamaalLogger(from: ContentView.self)
 
 struct ContentView: View {
     var body: some View {
+        NavigationStackView(
+            stack: [] as [Screens],
+            root: { screen in MainView(screen: screen) },
+            subView: { screen in MainView(screen: screen, displayMode: .inline) },
+            sidebar: { Sidebar() }
+        )
+        .onScreenChange { screen in
+            print("screen", screen)
+        }
+    }
+}
+
+struct HomeScreen: View {
+    var body: some View {
         VStack {
-            Button(action: { logger.info("Logged stuff") }) {
-                Image(systemName: "globe")
-                    .kBold()
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Hello, world!1111".digits)
+            Button(action: { logger.info("Logging something") }) {
+                Text("Loggin")
+            }
+            StackNavigationLink(
+                destination: Screens.other,
+                nextView: { screen in MainView(screen: screen, displayMode: .inline) }
+            ) {
+                Text("Go to other screen")
+            }
+        }
+    }
+}
+
+struct OtherScreen: View {
+    var body: some View {
+        StackNavigationBackButton(screenType: Screens.self) {
+            Text("Back")
+        }
+    }
+}
+
+struct MainView: View {
+    let screen: Screens
+    let displayMode: DisplayMode
+
+    init(screen: Screens, displayMode: DisplayMode = .large) {
+        self.screen = screen
+        self.displayMode = displayMode
+    }
+
+    var body: some View {
+        KJustStack {
+            switch screen {
+            case .home:
+                HomeScreen()
+            case .other:
+                OtherScreen()
+            }
+        }
+        .navigationTitle(title: screen.title, displayMode: displayMode)
+    }
+}
+
+struct Sidebar: View {
+    var body: some View {
+        List {
+            Section("Scenes") {
+                ForEach(Screens.allCases.filter(\.isSidebarItem), id: \.self) { screen in
+                    StackNavigationChangeStackButton(destination: screen) {
+                        Label(screen.title, systemImage: screen.imageSystemName)
+                            .foregroundColor(.accentColor)
+                    }
+                    .buttonStyle(.plain)
+                }
+            }
+        }
+        #if os(macOS)
+        .toolbar(content: {
+            Button(action: toggleSidebar) {
+                Label("Toggle sidebar", systemImage: "sidebar.left")
                     .foregroundColor(.accentColor)
             }
-            .buttonStyle(.plain)
-        }
-        .padding()
+        })
+        #endif
     }
+
+    #if os(macOS)
+    private func toggleSidebar() {
+        guard let firstResponder = NSApp.keyWindow?.firstResponder else { return }
+        firstResponder.tryToPerform(#selector(NSSplitViewController.toggleSidebar(_:)), with: nil)
+    }
+    #endif
+}
+
+enum Screens: Hashable, Codable, CaseIterable, NavigatorStackValue {
+    case home
+    case other
+
+    var title: String {
+        switch self {
+        case .home:
+            return "Home"
+        case .other:
+            return "Other"
+        }
+    }
+
+    var imageSystemName: String {
+        switch self {
+        case .home:
+            return "house.fill"
+        default:
+            return ""
+        }
+    }
+
+    var isTabItem: Bool {
+        switch self {
+        case .home:
+            return true
+        case .other:
+            return false
+        }
+    }
+
+    var isSidebarItem: Bool {
+        switch self {
+        case .home:
+            return true
+        case .other:
+            return false
+        }
+    }
+
+    static let root: Screens = .home
 }
 
 struct ContentView_Previews: PreviewProvider {
--- justfile
+++ justfile
@@ -10,3 +10,6 @@ format:
 
 lint:
     python3 Scripts/swiftlint_checker/main.py
+
+build-example:
+    set -o pipefail && xcodebuild -configuration Debug -workspace KamaalSwift.xcworkspace -scheme Example -destination "$DESTINATION" || exit 1
