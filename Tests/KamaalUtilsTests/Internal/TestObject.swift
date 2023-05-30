0b94dae78cc4514140115f2977e8111e6325712a
Kamaal <kamaal.f1@gmail.com>
2023-05-30 15:41:53 +0200

    Adding json file unpacker

---
+++ Tests/KamaalUtilsTests/Internal/TestObject.swift
@@ -0,0 +1,12 @@
+//
+//  TestObject.swift
+//
+//
+//  Created by Kamaal M Farah on 30/05/2023.
+//
+
+import Foundation
+
+struct TestObject: Codable, Equatable {
+    let value: String
+}
---
+++ Tests/KamaalUtilsTests/JSONFileUnpackerTests.swift
@@ -0,0 +1,43 @@
+//
+//  JSONFileUnpackerTests.swift
+//
+//
+//  Created by Kamaal M Farah on 30/05/2023.
+//
+
+import XCTest
+import Foundation
+import KamaalUtils
+
+final class JSONFileUnpackerTests: XCTestCase {
+    func testGetFileContent() throws {
+        let content = try JSONFileUnpacker<TestObject>(filename: "test", bundle: .module).content
+
+        XCTAssertEqual(content.value, "🐸FROG KINGDOM🐸")
+    }
+
+    func testFileNotFound() {
+        XCTAssertThrowsError(try JSONFileUnpacker<TestObject>(filename: "testy", bundle: .module)) { error in
+            let error = error as! JSONFileUnpackerErrors
+
+            XCTAssertEqual(error, .fileNotFound)
+        }
+    }
+
+    func testDecodingFailure() {
+        XCTAssertThrowsError(try JSONFileUnpacker<String>(filename: "test", bundle: .module)) { error in
+            let error = error as! JSONFileUnpackerErrors
+
+            let decodingError = DecodingError.typeMismatch(
+                String.self,
+                DecodingError
+                    .Context(
+                        codingPath: [],
+                        debugDescription: "Expected to decode String but found a dictionary instead.",
+                        underlyingError: nil
+                    )
+            )
+            XCTAssertEqual(error, .decodingFailure(context: decodingError))
+        }
+    }
+}
