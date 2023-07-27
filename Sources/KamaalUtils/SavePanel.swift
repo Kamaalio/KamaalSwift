d5cae68511bee7f779d908f8b7d107f83737249d
Kamaal Farah <kamaal.f1@gmail.com>
2023-07-27 14:06:24 +0200

    Adding a save panel util

---
+++ Sources/KamaalUtils/SavePanel.swift
@@ -0,0 +1,50 @@
+//
+//  SavePanel.swift
+//
+//
+//  Created by Kamaal M Farah on 27/07/2023.
+//
+
+#if os(macOS)
+import Cocoa
+
+public struct SavePanel {
+    private init() { }
+
+    public enum Errors: Error {
+        case canceled
+        case unknown(response: NSApplication.ModalResponse)
+    }
+
+    public static func save(filename: String) async -> Result<NSSavePanel, Errors> {
+        await withCheckedContinuation { continuation in
+            save(filename: filename) { result in
+                continuation.resume(returning: result)
+            }
+        }
+    }
+
+    private static func save(
+        filename: String,
+        beginHandler: @escaping (Result<NSSavePanel, Errors>) -> Void
+    ) {
+        DispatchQueue.main.async {
+            let panel = NSSavePanel()
+            panel.canCreateDirectories = true
+            panel.showsTagField = true
+            panel.nameFieldStringValue = filename
+            panel.level = NSWindow.Level(rawValue: Int(CGWindowLevelForKey(.modalPanelWindow)))
+            panel.begin(completionHandler: { result in
+                switch result {
+                case .cancel, .continue, .stop:
+                    beginHandler(.failure(.canceled))
+                case .OK:
+                    beginHandler(.success(panel))
+                default:
+                    beginHandler(.failure(.unknown(response: result)))
+                }
+            })
+        }
+    }
+}
+#endif
