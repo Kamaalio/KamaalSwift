abc0116d45d80d4c2bb78bc3f6077a1e7a303cb1
Kamaal Farah <kamaal.f1@gmail.com>
2023-08-31 22:08:59 +0200

    Initializing kamaal cloud target

---
+++ Sources/KamaalCloud/KamaalCloud+account.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Kamaal M Farah on 31/08/2023.
+//
+
+import Foundation
---
+++ Tests/KamaalCloudTests/KamaalCloudTests.swift
@@ -0,0 +1,11 @@
+//
+//  KamaalCloudTests.swift
+//
+//
+//  Created by Kamaal M Farah on 31/08/2023.
+//
+
+import XCTest
+import Foundation
+
+final class KamaalCloudTests: XCTestCase { }
