bdf1635013c5cb0581c19bf0d8637904c4db48af
Kamaal Farah <kamaal.f1@gmail.com>
2023-09-02 12:24:22 +0200

    Implementing fetch module

---
+++ Sources/KamaalCloud/Modules/CloudFetchModule.swift
@@ -0,0 +1,91 @@
+//
+//  CloudFetchModule.swift
+//
+//
+//  Created by Kamaal M Farah on 02/09/2023.
+//
+
+import CloudKit
+import Foundation
+import KamaalExtensions
+
+public class CloudFetchModule {
+    private let accounts: CloudAccountsModule
+    private let database: CKDatabase
+
+    init(accounts: CloudAccountsModule, database: CKDatabase) {
+        self.accounts = accounts
+        self.database = database
+    }
+
+    public enum Errors: Error {
+        case accountFailure(context: CloudAccountsModule.Errors)
+        case fetchFailure(context: Error)
+        case unknownFailure(context: Error?)
+    }
+
+    public func get(ofType objectType: String,
+                    by predicate: NSPredicate,
+                    limit: Int? = nil) async -> Result<[CKRecord], Errors> {
+        await withCheckedContinuation { continuation in
+            _fetch(ofType: objectType, by: predicate, limit: limit) { result in
+                continuation.resume(returning: result)
+            }
+        }
+    }
+
+    private func _fetch(ofType objectType: String,
+                        by predicate: NSPredicate,
+                        limit: Int? = nil,
+                        completion: @escaping (Result<[CKRecord], Errors>) -> Void) {
+        accounts.getStatus { [weak self] result in
+            guard let self else {
+                completion(.failure(.unknownFailure(context: nil)))
+                return
+            }
+
+            switch result {
+            case let .failure(failure):
+                completion(.failure(.accountFailure(context: failure)))
+                return
+            case .success: break
+            }
+
+            let query = CKQuery(recordType: objectType, predicate: predicate)
+            let queryOperation = CKQueryOperation(query: query)
+            if let limit {
+                queryOperation.resultsLimit = limit
+            }
+
+            var fetchedRecords: [CKRecord] = []
+            func fetchWithQueryOperation(_ operation: CKQueryOperation) {
+                operation.recordMatchedBlock = { _, recordResult in
+                    switch recordResult {
+                    case let .failure(failure): completion(.failure(.fetchFailure(context: failure)))
+                    case let .success(success): fetchedRecords = fetchedRecords.appended(success)
+                    }
+                }
+                operation.queryResultBlock = { operationResult in
+                    let cursor: CKQueryOperation.Cursor?
+                    switch operationResult {
+                    case let .failure(failure):
+                        completion(.failure(.fetchFailure(context: failure)))
+                        return
+                    case let .success(success): cursor = success
+                    }
+
+                    if let cursor {
+                        let cursorQueryOperation = CKQueryOperation(cursor: cursor)
+                        fetchWithQueryOperation(cursorQueryOperation)
+                        return
+                    }
+
+                    completion(.success(fetchedRecords))
+                }
+            }
+
+            fetchWithQueryOperation(queryOperation)
+            self.database.add(queryOperation)
+        }
+    }
+}
