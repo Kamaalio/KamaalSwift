b3ba6ebaf35a58b15425069a05722891741ee8d9
Kamaal Farah <kamaal.f1@gmail.com>
2023-09-02 14:57:54 +0200

    Adding a save operation

---
+++ Sources/KamaalCloud/Services/SaveService.swift
@@ -0,0 +1,31 @@
+//
+//  SaveService.swift
+//
+//
+//  Created by Kamaal M Farah on 02/09/2023.
+//
+
+import CloudKit
+import Foundation
+
+public enum SaveErrors: Error {
+    case saveFailure(context: Error)
+}
+
+class SaveService {
+    private let database: CKDatabase
+
+    init(database: CKDatabase) {
+        self.database = database
+    }
+
+    func save(record: CKRecord) async -> Result<CKRecord, SaveErrors> {
+        let updatedRecord: CKRecord
+        do {
+            updatedRecord = try await database.save(record)
+        } catch {
+            return .failure(.saveFailure(context: error))
+        }
+        return .success(updatedRecord)
+    }
+}
