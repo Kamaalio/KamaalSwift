e311d2f449951c3e38fabc05ed1a2bd863728a0b
Kamaal Farah <kamaal.f1@gmail.com>
2024-01-06 18:18:16 +0100

    Adding file clerk to manage files

---
+++ Sources/KamaalUtils/FileClerk.swift
@@ -0,0 +1,55 @@
+//
+//  FileClerk.swift
+//
+//
+//  Created by Kamaal M Farah on 06/01/2024.
+//
+
+import Foundation
+
+public struct FileClerk {
+    private static let fileManager = FileManager.default
+
+    private init() { }
+
+    public static let libraryDirectory = fileManager.urls(for: .libraryDirectory, in: .userDomainMask).first
+
+    @available(macOS 13.0, *)
+    public static func moveFolderContent(from sourceFolder: URL, to destinationFolder: URL) throws {
+        let sourceFolderContent = try fileManager.contentsOfDirectory(
+            at: sourceFolder,
+            includingPropertiesForKeys: nil,
+            options: []
+        )
+        for content in sourceFolderContent {
+            let destinationURL = destinationFolder.appending(path: content.lastPathComponent)
+            guard !self.fileManager.fileExists(atPath: destinationURL.path) else { continue }
+
+            try self.fileManager.moveItem(at: content, to: destinationURL)
+        }
+    }
+
+    public static func deleteItem(at url: URL) throws {
+        try self.fileManager.removeItem(at: url)
+    }
+
+    public static func getOrCreateDirectory(at url: URL) throws -> URL {
+        guard !self.fileManager.fileExists(atPath: url.path) else { return url }
+
+        try self.fileManager.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
+        return url
+    }
+
+    public static func createFile(with data: Data, at url: URL) {
+        self.fileManager.createFile(atPath: url.path, contents: data)
+    }
+
+    @available(macOS 13.0, *)
+    public static func getOrCreateSubfolder(atBase baseURL: URL, subFolderPaths: [String]) throws -> URL {
+        var currentPath = baseURL
+        for subFolderPath in subFolderPaths {
+            currentPath = try self.getOrCreateDirectory(at: currentPath.appending(path: subFolderPath))
+        }
+        return currentPath
+    }
+}
